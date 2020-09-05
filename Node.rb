5cecf85e2bbbb74e1a3ae187915f19a2b9cc9c39
Dmitriy Sidorov <d_sidorov@wargaming.net>
2020-09-05 21:36:17 +0300

    Init tree

---
+++ Node.rb
@@ -0,0 +1,7 @@
+class Node
+  def initialize(data)
+    self.data = data
+    self.left = nil
+    self.right = nil
+  end
+end
---
+++ Tree.rb
@@ -0,0 +1,18 @@
+class Tree
+  def initialize
+    self.root = nil
+  end
+
+  # function to be implemented 
+  # insert(data) 
+  # remove(data) 
+                
+
+  # Helper function 
+  # findMinNode() 
+  # getRootNode() 
+  # inorder(node) 
+  # preorder(node)                
+  # postorder(node) 
+  # search(node, data) 
+end
